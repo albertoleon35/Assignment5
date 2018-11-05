2f0eb1a663bec34b46596d36dcc7a54fff4de25d
Alberto Leon <albertoleon325@gmail.com>
2018-11-05 12:53:09 -0800

    Project 5 skeleton

---
+++ assignment5/assignment5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  assignment5
+//
+//  Created by Alberto Leon on 11/5/18.
+//  Copyright © 2018 Alberto Leon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
